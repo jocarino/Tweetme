f6dd8f9e488525f7db640ab29c123eb0fbc66f20
jocarino <joao.goncalo.veloso@gmail.com>
2020-09-10 20:34:01 +0100

    admin set up

--- Tweetme/tweets/admin.py
+++ Tweetme/tweets/admin.py
@@ -1,4 +1,14 @@
 from django.contrib import admin
 from .models import Tweet
 
-admin.site.register(Tweet)
+class TweetAdmin(admin.ModelAdmin):
+    #what to display when doing the look up (the Tweet __str__) can be changed in the model
+    list_display = ['__str__', 'user']
+    # user__username is user's username and etc
+    # look ups like Tweet.objects.filter(user__name/user__id=something)
+    search_fields = ['user__username', 'user__email']
+    class Meta:
+        model = Tweet
+
+
+admin.site.register(Tweet, TweetAdmin)
--- Tweetme/tweets/models.py
+++ Tweetme/tweets/models.py
@@ -9,6 +9,10 @@ class Tweet(models.Model):
     content = models.TextField(blank=True, null=True)
     # Image it's a media storage fiels that gets from the database
     image = models.FileField(upload_to='images/', blank=True, null=True)
+    """
+     def __str__(self):
+        return self.content
+     """
 
     class Meta:
         #Orders tweets in reverse order accoording to tweet id
