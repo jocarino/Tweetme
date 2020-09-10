a110739f6227917198f44f8718f7121f17f234d3
jocarino <joao.goncalo.veloso@gmail.com>
2020-09-10 20:21:45 +0100

    user foreign key in tweets

--- Tweetme/tweets/models.py
+++ Tweetme/tweets/models.py
@@ -1,7 +1,11 @@
 import random
+from django.conf import settings
 from django.db import models
 
+User = settings.AUTH_USER_MODEL
+
 class Tweet(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE) #many users can have many tweets
     content = models.TextField(blank=True, null=True)
     # Image it's a media storage fiels that gets from the database
     image = models.FileField(upload_to='images/', blank=True, null=True)
