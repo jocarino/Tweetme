9409977ce56b5fdaaaaf0d950097d72e4f710359
jocarino <joao.goncalo.veloso@gmail.com>
2020-09-12 16:23:27 +0100

    backend action : serializer and view

--- Tweetme/tweets/views.py
+++ Tweetme/tweets/views.py
@@ -10,7 +10,7 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from .models import Tweet
 from .forms import TweetForm
-from .serializers import TweetSerializer
+from .serializers import TweetSerializer, TweetActionSerializer
 
 ALLOWED_HOSTS = settings.ALLOWED_HOSTS
 
@@ -53,6 +53,33 @@ def tweet_delete_view(request, tweet_id, *args, **kwargs):
     obj.delete()
     return Response({"message": "Tweet delete."}, status=200)
 
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def tweet_action_view(request, *args, **kwargs):
+    """ 
+    id is required
+    Action option are: like, unlike and retweet
+    """
+    serializer = TweetActionSerializer(request.POST)
+    #validate action through the TweetAction serializer
+    if serializer.is_valid(raise_exception=True):
+        data = serializer.validated_data
+        #get serializer data
+        tweet_id = data.get("id")
+        action = data.get("action")
+
+        qs = Tweet.objects.filter(id=tweet_id)
+        if not qs.exists():
+            return Response({}, status=404) #tweet not found
+        obj = qs.first()
+        if action == "like":
+            obj.likes.add(request.user)
+        elif action == "unlike":
+            obj.likes.remove(request.user)
+        elif action == "retweet":
+            # this is todo
+            pass
+    return Response({"message": "Tweet delete."}, status=200)
 
 @api_view(['GET'])
 def tweet_list_view(request, *args, **kwargs):
