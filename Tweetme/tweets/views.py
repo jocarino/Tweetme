be3fa3f21b2b2c632ffee28c339242722a03ea2b
jocarino <joao.goncalo.veloso@gmail.com>
2020-09-12 10:55:46 +0100

    working serializer with response

--- Tweetme/tweets/views.py
+++ Tweetme/tweets/views.py
@@ -17,7 +17,8 @@ def home_view(request, *args, **kwargs):
 def tweet_create_view(request, *args, **kwargs):
     serializer = TweetSerializer(data=request.POST or None)
     if serializer.is_valid():
-        serializer.save()
+        obj = serializer.save(user=request.user)
+        return JsonResponse(serializer.data, status=201)
     return JsonResponse({}, status=400)
 
 def tweet_create_view_pure_django(request, *args, **kwargs):
